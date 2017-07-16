1ac0397f2483755caac79f76f1763b243e4ba61b
andaicheng <andaicheng@gmail.com>
2017-07-16 11:59:53 +0800

    Add utils

---
+++ modules/utils/src/main/java/com/andyadc/pomelo/utils/net/IPUtil.java
@@ -0,0 +1,14 @@
+package com.andyadc.pomelo.utils.net;
+
+import java.net.InetAddress;
+
+/**
+ * @author andaicheng
+ * @version 2017/1/8
+ */
+public class IPUtil {
+
+    private static InetAddress inetAddress;
+    private static String localHost;
+
+}
