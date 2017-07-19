3312d70da06f8a21c81190e585d389fce9e247fb
andaicheng <andaicheng@gmail.com>
2017-07-19 14:39:55 +0800

    Menagerie

---
+++ distributed/menagerie/src/main/java/com/andyadc/pomelo/menagerie/utils/ACLProvider.java
@@ -0,0 +1,26 @@
+package com.andyadc.pomelo.menagerie.utils;
+
+import org.apache.zookeeper.data.ACL;
+
+import java.util.List;
+
+/**
+ * @author andaicheng
+ * @version 2017/7/19
+ */
+public interface ACLProvider {
+    /**
+     * Return the ACL list to use by default (usually {@link org.apache.zookeeper.ZooDefs.Ids#OPEN_ACL_UNSAFE}).
+     *
+     * @return default ACL list
+     */
+    public List<ACL> getDefaultAcl();
+
+    /**
+     * Return the ACL list to use for the given path
+     *
+     * @param path path (NOTE: might be null)
+     * @return ACL list
+     */
+    public List<ACL> getAclForPath(String path);
+}
---
+++ distributed/menagerie/src/main/java/com/andyadc/pomelo/menagerie/utils/ZKPath.java
@@ -0,0 +1,8 @@
+package com.andyadc.pomelo.menagerie.utils;
+
+/**
+ * @author andaicheng
+ * @version 2017/7/19
+ */
+public class ZKPath {
+}
