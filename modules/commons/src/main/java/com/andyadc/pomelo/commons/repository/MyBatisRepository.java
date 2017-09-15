3e6869278301fd3a2407c45dda1196a5ff2a97d7
andyadc <andaicheng@gmail.com>
2017-09-15 15:13:42 +0800

    commons

---
+++ modules/commons/src/main/java/com/andyadc/pomelo/commons/repository/MyBatisRepository.java
@@ -0,0 +1,18 @@
+package com.andyadc.pomelo.commons.repository;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标识MyBatis的DAO,方便{org.mybatis.spring.mapper.MapperScannerConfigurer}的扫描。
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface MyBatisRepository {
+    String value() default "";
+}
